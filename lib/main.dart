99c6caab0e69515d9249377490615fa0cd566c11
aditya kumar <adityakumar1995rawat@gmail.com>
2021-11-12 22:53:09 +0530

    back4app connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,54 @@
+
 import 'package:bmi/screen/home.dart';
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
-import 'dart:async';
-
-void main() async  {
-  final keyaApplicationId = 'F2TfLD50KXcMV1MnJlKhOWj0tE2YORqgKzeIn0Cv';
+import 'dart:async';void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final keyApplicationId = 'F2TfLD50KXcMV1MnJlKhOWj0tE2YORqgKzeIn0Cv';
   final keyClientKey = 'jDPwbeR0vfKtvipac52kJgDhCuCpRHpCCVcyB5sK';
-  final keyParseServerUrl = 'https://parseapi.back4app.com/';
-  await Parse().initialize(keyaApplicationId, keyParseServerUrl,clientKey: keyClientKey,debug: true);
+  final keyParseServerUrl = 'https://parseapi.back4app.com';
 
-  runApp(MyApp());
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, autoSendSessionId: true);
+
+  var firstObject = ParseObject('FirstClass')
+    ..set(
+        'message', 'Hey ! First message from Flutter. Parse is now connected');
+  await firstObject.save();
+  
+  print('done');
+    runApp(MyApp());
+  
 }
-  Future<String> saveNewPlayer() 
-  async {
-    final soccerPlayer = ParseObject('SoccerPlayer')
-    ..set('playerName', 'A. Wed')
-    ..set('yaerOfBirth', 1997)
-    ..set('emailCOntact', 'a.wed@gmial.com')
-    ..set('attributes', ['fast','good conditioning']);
-    await soccerPlayer.save();
-    return soccerPlayer.objectId;
-  }
+
+
+
+
+// void main() async  {
+//   final keyApplicationId = 'F2TfLD50KXcMV1MnJlKhOWj0tE2YORqgKzeIn0Cv';
+//   final keyClientKey = 'jDPwbeR0vfKtvipac52kJgDhCuCpRHpCCVcyB5sK';
+//   final keyParseServerUrl = 'https://parseapi.back4app.com/';
+//     await Parse().initialize(keyApplicationId, keyParseServerUrl,
+//       clientKey: keyClientKey, autoSendSessionId: true);
+
+//   var firstObject = ParseObject('FirstClass')
+//     ..set(
+//         'message', 'Hey ! First message from Flutter. Parse is now connected');
+//   await firstObject.save();
+  
+//   print('done');
+//   runApp(MyApp());
+// }
+//   Future<String> saveNewPlayer() 
+//   async {
+//     final soccerPlayer = ParseObject('SoccerPlayer')
+//     ..set('playerName', 'A. Wed')
+//     ..set('yaerOfBirth', 1997)
+//     ..set('emailCOntact', 'a.wed@gmial.com')
+//     ..set('attributes', ['fast','good conditioning']);
+//     await soccerPlayer.save();
+//     return soccerPlayer.objectId;
+//   }
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
